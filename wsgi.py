ebfa17dbbad6811f6e814fd270c3f85526133db0
Anusree.M <95077280+Anusreemmm@users.noreply.github.com>
2024-09-30 11:53:44 +0530

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Ebook project.
+WSGI config for Ebook_summery project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ebook.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ebook_summery.settings')
 
 application = get_wsgi_application()
